e8776fee757b4d16bfb6bc787883a8993a13d8fa
dagoldenburg <dagoldenburg@gmail.com>
2017-12-10 23:02:19 +0100

    uppdaterad

---
+++ src/BO/RestClient.java
@@ -0,0 +1,21 @@
+package BO;
+
+import UI.UserViewModel;
+import com.google.gson.Gson;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+
+public class RestClient {
+    private static final String userUrl ="http://localhost:8080/user/";
+    private  static Client client  = ClientBuilder.newClient();
+    private static Gson gson =  new Gson();
+
+
+    public static UserViewModel getUserById(long id){
+        String value =client.target(userUrl+"user").path(String.valueOf(id)).request().accept(MediaType.APPLICATION_JSON_TYPE).get(String.class);
+        UserViewModel userViewModel =gson.fromJson(value, UserViewModel.class);
+        return userViewModel;
+    }
+}
--- src/main.java
+++ src/main.java
@@ -1,18 +1,7 @@
-import BO.Facade;
 import BO.Message;
 import DB.MessageDb;
-import DB.UserDb;
-import UI.UserViewModel;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Form;
-import javax.ws.rs.core.MediaType;
-import java.util.LinkedList;
 import java.util.List;
-import org.glassfish.jersey.client.ClientConfig;
 
 
 /**
